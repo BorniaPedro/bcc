1300237cdfab19879305395e332ecf118193f920
Pedro Bornia <borniaph@gmail.com>
2024-04-19 23:19:37 -0300

    PRU update

---
+++ Engenharia de Software/Principios SOLID/Princípio da Responsabilidade Única/PRUerrado.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Turma{
+    private:
+     float taxaAprovacao;
+    public:
+     void calculaAprovacao(int aprovados, int totalAlunos){
+         taxaAprovacao = (aprovados / totalAlunos) * 100;  
+         cout << "Taxa de Aprovacao: " << taxaAprovacao << "%" << endl;
+     }
+}
+
+int main(){
+    Turma t1;
+    t1.calculaAprovacao(10, 100);
+    return 0;
+}
+
+
+
